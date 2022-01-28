f2040a6b7c3c6696d46cf14134fa32ad03974198
Antonio Villanueva Segura <karpvs@yahoo.fr>
2022-01-28 16:04:50 +0100

    Cuarta Version Una segunda corrutina lanza una supervision de la creacion de lineas antes de escribir en el TextView ahora , asi no planta

--- app/src/main/java/com/example/testip/MainActivity.kt
+++ app/src/main/java/com/example/testip/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import com.example.testip.databinding.ActivityMainBinding
 import android.content.Context
 import android.net.wifi.WifiManager
+import android.os.Handler
+import android.os.Looper
 import android.text.format.Formatter
 import android.text.method.ScrollingMovementMethod
 import kotlinx.coroutines.*
@@ -15,8 +17,13 @@ import java.net.*
 
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
+    var tmp="" //Texto temporal contiene las IPs
+    var lineasCreadas=0
+    var tempo=0
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,9 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(view)
 
-
-        val ip1 = binding.etiquetteIp
-        //ip1.setText("192.168.6.1")
+        val ip1 = binding.etiquetteIp //Ip aparato
 
         val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
 
@@ -34,78 +39,101 @@ class MainActivity : AppCompatActivity() {
         ip1.setText("IP ADDRESS " + ipAddress)
 
 
+        binding.ipListe.movementMethod = ScrollingMovementMethod.getInstance()
 
-        binding.buttonScanner.setOnClickListener {
-            binding.editTextTextMultiLineIp.setText(" ")
-            binding.buttonScanner.setText("SEARCHING")
-            try {
-                testIpRange(ipAddress,wifiManager)
-            }catch (e:Exception){
-                println ("ERROR = "+e.toString())
-            }
+        var button=binding.buttonScanner
 
-        }
+       // binding.ipListe.movementMethod=ScrollingMovementMethod ()
+        button.setOnClickListener {
+            binding.ipListe.setText(" .... searching ...")
+            //button.setText("SEARCHING")
 
-    }
+            testIpRange(ipAddress) //Analiza ips de 0 .. 255
 
 
-    fun testIpRange(ip_: String,wifiManager: WifiManager) {
+            /*Segundo scope Evita crash TextView && scrollBar && corutina
+            * Cuando no se crean nuevas lineas se copia el fichero temporal
+            * con las ip temp en el TextView
+            */
+            GlobalScope.launch(Dispatchers.Main) {
+                withContext(Dispatchers.IO) {
 
+                    while (tempo<10) { //Un tiempo antes de escribir en TextView no hay nuevas lineas
+                        delay(10) //Retardo tonto
 
-        var existe: Boolean = false
-        var ListIp = binding.editTextTextMultiLineIp
-
-        var ip = ip_ //Recupera ip en dispositivo Android
-
-        //Cut last value  in ip  ip[3]
-        ip = ip.subSequence(0, ip.lastIndexOf(".")).toString()
-
-        for (elem in 0..254) { //Recorre ips de 0 .. 255
-
-            val ip_actual = ip + "." + elem.toString() //Ip actual
-
-
-                    GlobalScope.launch(Dispatchers.Main) {
-
-                        withContext(Dispatchers.IO) {
-
-
-                                existe = ipExist(ip_actual)
-
-                            ListIp.setText( //Actualiza view ip actual
-                                ListIp.getText().toString() + '\n' + ip_actual+"..."+existe.toString())
-
-                            /*
-                                if (existe) {
-
-                                    ListIp.setText( //Actualiza view ip actual
-                                        ListIp.getText().toString() + '\n' + ip_actual
-
-                                    )
-
-
-                                }
-                            */
+                        if (lineasCreadas < cuentaLineasCreadas(tmp)) {
+                            lineasCreadas = cuentaLineasCreadas(tmp)
+                            tempo = 0 //Reset nueva linea
+                        } else {
+                            tempo++
+                            println ("Tempo "+tempo.toString())
                         }
 
                     }
 
 
+                    //TextView (tmp)
+                    binding.ipListe.setText(tmp)
+
+                    //delay (1000)
+                    //Restaura boton
+                    //button.setText("scanner")
+                }
+
+            }
+
 
         }
 
+
+    }
+    //Analiza un rango de IPs desde la IP base en mascara 255.255.255.0..255
+fun testIpRange(ip_: String) {
+
+    var ListIp = binding.ipListe
+
+    var ip = ip_ //Recupera ip en dispositivo Android
+
+    //Cut last value  in ip  ip[3]
+    ip = ip.subSequence(0, ip.lastIndexOf(".")).toString()
+
+    for (elem in 0..255) { //Recorre ips de 0 .. 255
+
+        val ip_actual = ip + "." + elem.toString() //Ip actual
+
+        GlobalScope.launch(Dispatchers.Main) {
+            withContext(Dispatchers.IO) {
+
+                var existe: Boolean
+                existe = ipExist(ip_actual)
+                /*Antes de escribir en el TextView se guarda en un txt temporal
+                *Una segunda corrutina se encarga de escribir en el TextView
+                * pasado un tiempo sin nuevas lineas
+                 */
+                tmp += ip_actual + "..." + existe.toString() + '\n'
+
+            }
+        }
     }
 
+    }
 
+    //Cuenta la cantidad de lineas creadas en txt
+    fun cuentaLineasCreadas (txt:String):Int{
+        var count=0
+        for (elem in txt.lines()){
+            count++
+        }
+        return count
+    }
+
+    //Existe la ip ..ping -c 1 host ?
     fun ipExist(host: String): Boolean {
 
         val runtime = Runtime.getRuntime()
         try {
-
-            val ipProcess = runtime.exec("/system/bin/ping -c 1 $host")
-
+            val ipProcess = runtime.exec("/system/bin/ping -c 1  $host")
             val exitValue = ipProcess.waitFor()
-
             ipProcess.destroy()
             return exitValue == 0
 
@@ -117,7 +145,6 @@ class MainActivity : AppCompatActivity() {
 
         } catch (e: InterruptedException) {
             e.printStackTrace()
-
         }
 
         return false
